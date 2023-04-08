8fb60a1266833d515eb13cdecc8a6e46373b9383
Gegham <gexam.karapetyan2000@gmail.com>
2023-04-08 10:46:53 +0400

    added alchemy_simulateAssetChangesBundle

--- alchemy/common.go
+++ alchemy/common.go
@@ -8,31 +8,60 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-func PrintAssetChangeRequest(chainId uint,
-	interactAddress common.Address,
-	fromAddress common.Address,
-	etherValue *big.Int,
-	callData []byte,
-	gasLimit uint64,
-	gasPrice *big.Int) {
+type RequestBody struct {
+	//ChainId         uint
+	InteractAddress common.Address
+	FromAddress     common.Address
+	EtherValue      *big.Int
+	CallData        []byte
+	GasLimit        uint64
+	GasPrice        *big.Int
+}
 
-	endpointPart := ""
+func DecideAlchemyEndpoint(chainId uint) string {
 	if chainId == 1 {
-		endpointPart = "eth-mainnet"
+		return "eth-mainnet"
 	} else if chainId == 137 {
-		endpointPart = "polygon-mainnet"
+		return "polygon-mainnet"
 	} else if chainId == 5 {
-		endpointPart = "eth-goerly"
+		return "eth-goerly"
 	} else if chainId == 80001 {
-		endpointPart = "polygon-mumbai"
+		return "polygon-mumbai"
 	} else {
 		panic(errors.New(fmt.Sprintf("alchemy endpoint undefined for %d", chainId)))
 	}
+}
 
-	post := fmt.Sprintf(`{"id": 1, "jsonrpc": "2.0", "method":"alchemy_simulateAssetChanges","params": [{"from": "%s", "to": "%s", "value": "0x%x", "data": "0x%x", "gas": "0x%x", "gasPrice": "0x%x"}]}`, fromAddress, interactAddress, etherValue, callData, gasLimit, gasPrice)
+func PrintAssetChangeRequest(chainId uint, obj RequestBody) {
+	endpointPart := DecideAlchemyEndpoint(chainId)
+
+	post := fmt.Sprintf(`{"id": 1, "jsonrpc": "2.0", "method":"alchemy_simulateAssetChanges","params": [{"from": "%s", "to": "%s", "value": "0x%x", "data": "0x%x", "gas": "0x%x", "gasPrice": "0x%x"}]}`,
+		obj.FromAddress, obj.InteractAddress, obj.EtherValue, obj.CallData, obj.GasLimit, obj.GasPrice)
 
 	curl := fmt.Sprintf(`curl https://%s.g.alchemy.com/v2/docs-demo -H 'Origin: https://docs.alchemy.com' -H 'accept: application/json' -H 'content-type: application/json' --data-binary '%s'`, endpointPart, post)
 
 	fmt.Println("run the following command in terminal to send request to alchemy")
 	fmt.Println(curl)
+}
+
+func PrintAssetChangeBundleRequest(chainId uint, obj []RequestBody) {
+	//var result string
+	var post []string
+	post = append(post, fmt.Sprintf(`{"id": 1, "jsonrpc": "2.0", "method":"alchemy_simulateAssetChanges"}`))
+
+	endpointPart := DecideAlchemyEndpoint(chainId)
+	for i := 0; i < len(obj); i++ {
+		// post = fmt.Sprintf(`{"id": 1, "jsonrpc": "2.0", "method":"alchemy_simulateAssetChanges","params": [[{"from": "%s", "to": "%s", "value": "0x%x", "data": "0x%x", "gas": "0x%x", "gasPrice": "0x%x"}]]}`,
+		// 	obj[i].FromAddress, obj[i].InteractAddress, obj[i].EtherValue, obj[i].CallData, obj[i].GasLimit, obj[i].GasPrice)
+		// result += post
+
+		post = append(post, fmt.Sprintf(`{"params": [{"from": "%s", "to": "%s", "value": "0x%x", "data": "0x%x", "gas": "0x%x", "gasPrice": "0x%x"}]}`,
+			obj[i].FromAddress, obj[i].InteractAddress, obj[i].EtherValue, obj[i].CallData, obj[i].GasLimit, obj[i].GasPrice))
+	}
+
+	curl := fmt.Sprintf(`curl https://%s.g.alchemy.com/v2/docs-demo -H 'Origin: https://docs.alchemy.com' -H 'accept: application/json' -H 'content-type: application/json' --data-binary '%s'`, endpointPart, post)
+
+	fmt.Println("run the following command in terminal to send request to alchemy")
+	fmt.Println(curl)
+
 }
--- main.go
+++ main.go
@@ -5,14 +5,14 @@ import (
 	"goland/go2web3/alchemy"
 	"goland/go2web3/erc20Transaction"
 	"goland/go2web3/ethTransaction"
-	"goland/go2web3/uni"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func main() {
 
-	client, chainId, err := ethTransaction.PolygonMainnetClient()
+	client, chainId, err := ethTransaction.PolygonMumbaiClient()
 	if nil != err {
 		panic(err)
 	}
@@ -49,23 +49,29 @@ func main() {
 	// 	common.HexToAddress("0xE592427A0AEce92De3Edee1F18E0157C05861564"),
 	// 	10)
 
-	wmatic := erc20Transaction.WrappedMatic(chainId)
-	usdc := erc20Transaction.USDC(chainId)
+	// wmatic := erc20Transaction.WrappedMatic(chainId)
+	// usdc := erc20Transaction.USDC(chainId)
+	toAddress := common.HexToAddress("0x827C81e18a1b729Da4C0bf20c1Dedd0EC6dA2182")
+
+	err, callData, interactAddress, gasLimit, etherValue := erc20Transaction.Transfer(client,
+		privateKey, common.HexToAddress("0x18d223fe5d767b2db7586ad23d8134dd8990fa47"), toAddress, 100)
 
 	//fmt.Println(chainId, usdc.Address, wmatic.Address)
 
-	err, callData, interactAddress, poolAddress, gasLimit, etherValue := uni.Swap(client,
-		crypto.PubkeyToAddress(privateKey.PublicKey),
-		100, // 0.01%
-		usdc,
-		1,
-		wmatic)
+	//fmt.Println(crypto.PubkeyToAddress(privateKey.PublicKey))
+
+	//err, callData, interactAddress, poolAddress, gasLimit, etherValue := uni.Swap(client,
+	// crypto.PubkeyToAddress(privateKey.PublicKey),
+	// 100, // 0.01%
+	// usdc,
+	// 1,
+	// wmatic)
 
 	if nil != err {
 		panic(err)
 	}
 
-	fmt.Printf("Pool Address: %s\n\n", poolAddress)
+	//fmt.Printf("Pool Address: %s\n\n", poolAddress)
 
 	err, transactionJSON, hash, gasPrice := ethTransaction.SignTransaction(client, privateKey, interactAddress, gasLimit, callData, chainId, etherValue)
 
@@ -76,5 +82,32 @@ func main() {
 	fmt.Printf("JSON transaction: %s\n\n", transactionJSON)
 	fmt.Printf("Transaction hash: %s\n\n", hash)
 
-	alchemy.PrintAssetChangeRequest(chainId, interactAddress, crypto.PubkeyToAddress(privateKey.PublicKey), etherValue, callData, gasLimit, gasPrice)
+	ob := []alchemy.RequestBody{
+		{
+			InteractAddress: interactAddress,
+			FromAddress:     crypto.PubkeyToAddress(privateKey.PublicKey),
+			EtherValue:      etherValue,
+			CallData:        callData,
+			GasLimit:        gasLimit,
+			GasPrice:        gasPrice,
+		},
+		{
+			InteractAddress: interactAddress,
+			FromAddress:     crypto.PubkeyToAddress(privateKey.PublicKey),
+			EtherValue:      etherValue,
+			CallData:        callData,
+			GasLimit:        gasLimit,
+			GasPrice:        gasPrice,
+		},
+		{
+			InteractAddress: interactAddress,
+			FromAddress:     crypto.PubkeyToAddress(privateKey.PublicKey),
+			EtherValue:      etherValue,
+			CallData:        callData,
+			GasLimit:        gasLimit,
+			GasPrice:        gasPrice,
+		},
+	}
+
+	alchemy.PrintAssetChangeBundleRequest(chainId, ob) //chainId, interactAddress, crypto.PubkeyToAddress(privateKey.PublicKey), etherValue, callData, gasLimit, gasPrice
 }
